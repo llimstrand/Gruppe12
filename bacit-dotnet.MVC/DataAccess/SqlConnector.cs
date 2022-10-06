8a746902135f974bb837af1f188da2324080a196
siljestrm <siljees@uia.no>
2022-10-06 14:48:34 +0200

    Endra alle steder det sto For_noe til Sug_noe

--- bacit-dotnet.MVC/DataAccess/SqlConnector.cs
+++ bacit-dotnet.MVC/DataAccess/SqlConnector.cs
@@ -46,7 +46,7 @@ namespace bacit_dotnet.MVC.DataAccess
         {
             using var connection = new MySqlConnection(config.GetConnectionString("MariaDB"));
             connection.Open();
-            var query = "Insert into suggestions(For_Overskrift, For_Beskrivelse, For_Ansvarlig, For_Status, For_Frist, For_Varighet, For_Timestamp) values (@Overskrift, @Beskrivelse, @Ansvarlig, @Status, @Frist, @Varighet, @Timestamp)";
+            var query = "Insert into suggestions(Sug_Overskrift, Sug_Beskrivelse, Sug_Ansvarlig, Sug_Status, Sug_Frist, Sug_Varighet, Sug_Timestamp) values (@Overskrift, @Beskrivelse, @Ansvarlig, @Status, @Frist, @Varighet, @Timestamp)";
             Console.WriteLine(query);
             WriteData(query, connection, model);
             connection.Close();
@@ -59,13 +59,13 @@ namespace bacit_dotnet.MVC.DataAccess
             using var command = conn.CreateCommand();
             command.CommandType = System.Data.CommandType.Text;
             command.CommandText = query;
-            command.Parameters.AddWithValue("@Overskrift", model.For_Overskrift);
-            command.Parameters.AddWithValue("@Beskrivelse", model.For_Beskrivelse);
-            command.Parameters.AddWithValue("@Ansvarlig", model.For_Ansvarlig);
-            command.Parameters.AddWithValue("@Status", model.For_Status);
-            command.Parameters.AddWithValue("@Frist", model.For_Frist);
-            command.Parameters.AddWithValue("@Varighet", model.For_Varighet);
-            command.Parameters.AddWithValue("@Timestamp", model.For_Timestamp);
+            command.Parameters.AddWithValue("@Overskrift", model.Sug_Overskrift);
+            command.Parameters.AddWithValue("@Beskrivelse", model.Sug_Beskrivelse);
+            command.Parameters.AddWithValue("@Ansvarlig", model.Sug_Ansvarlig);
+            command.Parameters.AddWithValue("@Status", model.Sug_Status);
+            command.Parameters.AddWithValue("@Frist", model.Sug_Frist);
+            command.Parameters.AddWithValue("@Varighet", model.Sug_Varighet);
+            command.Parameters.AddWithValue("@Timestamp", model.Sug_Timestamp);
             command.ExecuteNonQuery(); 
         }
     }
--- bacit-dotnet.MVC/Models/Suggestions/SuggestionViewModel.cs
+++ bacit-dotnet.MVC/Models/Suggestions/SuggestionViewModel.cs
@@ -6,19 +6,19 @@ namespace bacit_dotnet.MVC.Models.Suggestions
     {
         [Required]
         [MinLength(7, ErrorMessage ="Skriv en ordentlig tittel!")]
-        public string For_Overskrift { get; set; }
-        public string For_Beskrivelse { get; set; }
-        public string For_Ansvarlig { get; set; }
-        public string For_Status { get; set; }
-        public string For_Frist { get; set; }
-        public string For_Varighet { get; set; }
-        public byte[] For_VedleggEn { get; set; }
-        public byte[] For_VedleggTo { get; set; }
+        public string Sug_Overskrift { get; set; }
+        public string Sug_Beskrivelse { get; set; }
+        public string Sug_Ansvarlig { get; set; }
+        public string Sug_Status { get; set; }
+        public string Sug_Frist { get; set; }
+        public string Sug_Varighet { get; set; }
+        public byte[] Sug_VedleggEn { get; set; }
+        public byte[] Sug_VedleggTo { get; set; }
 
-        public DateTime For_Timestamp {get; set;}
+        public DateTime Sug_Timestamp {get; set;}
 
         public SuggestionViewModel(){
-            this.For_Timestamp = DateTime.Now;
+            this.Sug_Timestamp = DateTime.Now;
         }
         
     }

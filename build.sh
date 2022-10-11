723021ab7a90ee0d1980476b3a2f4467b17cfc2f
emilie <emiliebs@uia.no>
2022-10-11 15:11:06 +0200

    escape

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 @echo off
 
-:: Kill running instance of container
+#Kill running instance of container
 docker kill webapp
 
-:: Builds image specified in Dockerfile
+#Builds image specified in Dockerfile
 docker image build -t webapp .
 
-:: Starts container with web application and maps port 80 (ext) to 80 (internal)
+#Starts container with web application and maps port 80 (ext) to 80 (internal)
 docker container run --rm -it -d --name webapp --publish 80:80 webapp
 
 echo.

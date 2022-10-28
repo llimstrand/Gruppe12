132e5c6256d5d3153fd70ce0020633b4c0f9ab4b
cathrinerh <cathrine_roestad@hotmail.no>
2022-10-28 10:52:21 +0200

    Fikset DateTime

--- bacit-dotnet.MVC/DataAccess/SqlConnector.cs
+++ bacit-dotnet.MVC/DataAccess/SqlConnector.cs
@@ -123,10 +123,9 @@ namespace bacit_dotnet.MVC.DataAccess
             connection.Open();
 
             var Suggestions = new List<Suggestion>();
-            var reader = ReadData("select Sug_ID, Sug_Overskrift, Sug_Beskrivelse, Sug_Ansvarlig, Sug_Status, Sug_Frist, Sug_Varighet, Sug_TimeStamp from suggestions", connection);
+            var reader = ReadData("select Sug_ID, Sug_Overskrift, Sug_Beskrivelse, Sug_Ansvarlig, Sug_Status, Sug_Frist, Sug_Varighet from suggestions", connection);
             while (reader.Read())
             {
-                DateTime date = reader.GetDateTime("Sug_Timestamp");
 
                 var user = new Suggestion();
                 user.Sug_ID = reader.GetInt32("Sug_ID");
@@ -136,7 +135,6 @@ namespace bacit_dotnet.MVC.DataAccess
                 user.Sug_Status = reader.GetString("Sug_Status");
                 user.Sug_Frist = reader.GetString("Sug_Frist");
                 user.Sug_Varighet = reader.GetString("Sug_Varighet");
-                user.Sug_Timestamp = date.ToString("D");
 
             
                 Suggestions.Add(user);
